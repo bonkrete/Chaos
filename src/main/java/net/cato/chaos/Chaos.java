a12282c0b9fc3d6aae36c581367d55b51738faf2
BuildTools <unconfigured@null.spigotmc.org>
2023-04-30 22:39:24 -0500

    Initial commit

---
+++ src/main/java/net/cato/chaos/Chaos.java
@@ -0,0 +1,13 @@
+package net.cato.chaos;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Chaos implements ModInitializer {
+	public static final String MOD_ID = "chaos";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/net/cato/chaos/ChaosClient.java
@@ -0,0 +1,10 @@
+package net.cato.chaos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChaosClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
